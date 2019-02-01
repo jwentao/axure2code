4f9842bdb1a87bfd4296cb3c166ed4be11d96a77
jwentao <jinwentao@huya.com>
2019-02-01 18:16:13 +0800

    commit

--- config/proxyConfig.js
+++ config/proxyConfig.js
@@ -5,7 +5,7 @@ module.exports = {
       // target: 'http://172.20.50.3',
       target: 'http://6qfaaa.axshare.com',
       // target: 'http://test-bizinfo.huya.com',
-      secure: false,  // 如果是https接口，需要配置这个参数
+      secure: true,  // 如果是https接口，需要配置这个参数
       changeOrigin: true,  //是否跨域
       pathRewrite: {
         '^/axshare': ''   //需要rewrite的,
--- src/components/pages/index.vue
+++ src/components/pages/index.vue
@@ -10,8 +10,8 @@
   export default {
     name: 'index',
     async mounted () {
-      let data = await api.getHtml('/axshare/#p=%E5%BA%94%E7%94%A8%E7%A9%BA%E9%97%B4-%E6%AD%A3%E5%B8%B8%E8%BF%90%E8%90%A5&g=1');
-      console.log(data);
+      // let data = await api.getHtml('/axshare/#p=%E5%BA%94%E7%94%A8%E7%A9%BA%E9%97%B4-%E6%AD%A3%E5%B8%B8%E8%BF%90%E8%90%A5&g=1');
+      // console.log(data);
     }
   };
 </script>
